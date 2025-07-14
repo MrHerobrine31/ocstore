def596b86d3338a380a23589fdf9afde02d69e74
MrHerobrine31 <2008yaroslav2008@gmail.com>
2025-07-14 16:32:51 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/projects/ocstore/admin/');
+define('HTTP_CATALOG', 'http://localhost/projects/ocstore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/projects/ocstore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/projects/ocstore/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/laragon/www/projects/ocstore/admin/');
+define('DIR_SYSTEM', 'D:/laragon/www/projects/ocstore/system/');
+define('DIR_IMAGE', 'D:/laragon/www/projects/ocstore/image/');
+define('DIR_STORAGE', 'D:/laragon/storage/');
+define('DIR_CATALOG', 'D:/laragon/www/projects/ocstore/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocstore');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
