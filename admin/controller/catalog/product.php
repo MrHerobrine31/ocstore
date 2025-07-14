f2506c362564d067c50b45868e2799e2ecfc8a1f
MrHerobrine31 <2008yaroslav2008@gmail.com>
2025-07-14 16:41:37 +0300

    123456

--- admin/controller/catalog/product.php
+++ admin/controller/catalog/product.php
@@ -1,6 +1,7 @@
 <?php
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
+// * 123
 
 class ControllerCatalogProduct extends Controller {
 	private $error = array();
